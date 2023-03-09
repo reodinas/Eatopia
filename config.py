85b36856b8b4b2607dcffa79b9927c5d27386331
Choi Donghyeok <120348461+reodinas@users.noreply.github.com>
2023-03-09 17:57:26 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 
 class Config :
-    HOST = 'yh-db.cdtkthbhsama.ap-northeast-2.rds.amazonaws.com'
-    DATABASE = 'movie_db2'
-    DB_USER = 'movie_db_user'
-    DB_PASSWORD = 'yh1234db'
-    SALT = 'friday'
+    HOST = ''
+    DATABASE = ''
+    DB_USER = ''
+    DB_PASSWORD = ''
+    SALT = ''
 
     # JWT 관련 변수 셋팅
-    JWT_SECRET_KEY = 'moviemovie'
+    JWT_SECRET_KEY = ''
     JWT_ACCESS_TOKEN_EXPIRES = False 
     
     PROPAGATE_EXCEPTIONS = True # JWT 에러메시지를 명시함
